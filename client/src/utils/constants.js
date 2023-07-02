7a4d9b1a112c4fe9d755f4f97d90aa21f70e885a
Aman <amanvarshney.varshney@gmail.com>
2023-07-02 20:27:16 +0530

    Updated PRODUCTION_URL

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const LOCALSTORAGE_TOKEN_KEY = "__xseed_education_token__";
-export const PRODUCTION = false; // production // CHANGE THIS
-export const PRODUCTION_URL = "";
+export const PRODUCTION = true; // production // CHANGE THIS
+export const PRODUCTION_URL = "https://xseed-api.onrender.com/api/v1";
 export const LOCAL_URL = "http://localhost:5000/api/v1";
